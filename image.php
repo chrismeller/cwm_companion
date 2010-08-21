5c9bb0609861adbd9c12acf4d491c61fb83615bc
Chris Meller <chris@doesnthaveone.com>
2010-08-21 16:35:47 -0400

    Switch the dynamic header image loading into an AJAX method so it will continue to be dynamic even when the page is cached.

---
+++ image.php
@@ -0,0 +1,35 @@
+<?php
+
+	$headers = array(
+		array(
+			'img' => 'images/headers/header_sign.jpg',
+			'flickr' => 'http://www.flickr.com/photos/mellertime/4485964682/',
+			'width' => 950,
+			'height' => 250,
+		),
+		array(
+			'img' => 'images/headers/header_duckies.jpg',
+			'flickr' => 'http://www.flickr.com/photos/mellertime/4710923546/',
+			'width' => 950,
+			'height' => 250,
+		),
+		array(
+			'img' => 'images/headers/header_pier.jpg',
+			'flickr' => 'http://www.flickr.com/photos/mellertime/4485317853/',
+			'width' => 950,
+			'height' => 250,
+		),
+	/*	array(
+			'img' => 'images/headers/header_lake.jpg',
+			'flickr' => 'http://www.flickr.com/photos/mellertime/3576628927/',
+			'width' => 950,
+			'height' => 250,
+		), */
+	);
+	
+	// pick a header
+	$header = $headers[ mt_rand( 0, count( $headers ) - 1 ) ];
+	
+	echo json_encode( $header );
+
+?>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 jQuery(document).ready( function () {
 	
+	jQuery.getJSON( CWM.template_directory + '/image.php', function(data) {
+		jQuery('#banner').css('background', 'url("' + CWM.template_directory + '/' + data.img + '") no-repeat');
+		jQuery('#banner').css('width', data.width + 'px');
+		jQuery('#banner').css('height', data.height + 'px');
+		
+		jQuery('#banner .flickr_link').attr('href', data.flickr);
+	} );
+	
 	jQuery('#search input').focus( function() {
 		jQuery(this).addClass('focus');
 		jQuery(this).val('');
