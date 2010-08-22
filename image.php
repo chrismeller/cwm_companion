a19bac5b0b21ae387553f586db99054d3cd084a7
chris <chris@9841916d-1f77-4d57-ab71-7f08b6dd89e2>
2010-08-22 22:48:38 +0000

    Add a header of the pier with waves to the rotation.
    git-svn-id: https://svn.chrismeller.com/cwm/branches/chris-mkii@104 9841916d-1f77-4d57-ab71-7f08b6dd89e2

--- image.php
+++ image.php
@@ -43,6 +43,12 @@
 			'width' => 950,
 			'height' => 250,
 		),
+		array(
+			'img' => 'images/headers/header_pier_waves.jpg',
+			'flickr' => 'http://www.flickr.com/photos/mellertime/4485970534/',
+			'width' => 950,
+			'height' => 250,
+		),
 	);
 	
 	// pick a header
