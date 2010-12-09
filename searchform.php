799823d305692b3cb2e0f78adc4a583d414c23e5
Chris Meller <chris@doesnthaveone.com>
2010-12-09 14:08:14 -0500

    Delete all the files from this branch that were copied over from the WordPress theme.

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url(); ?>">
-	<label class="screen-reader-text" for="s"><?php __('Search for:'); ?></label>
-	<div class="left"></div>
-		<input type="text" name="s" id="s" value="<?php echo CWM::search(); ?>" />
-	<div class="right"></div>
-	<input type="submit" id="searchsubmit" value="<?php esc_attr__('Search'); ?>" />
-</form>
